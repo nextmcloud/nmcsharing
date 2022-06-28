82b8ff17c450e0419fb78eaa87f170d3a87fa800
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2022-06-28 13:25:34 +0530

    Show pending shares within the file list view

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ *
+ * @author Kavita sonawane<kavita.sonawane@t-systems.com>
+
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\Nmcsharing\AppInfo;
+
+use OCA\DAV\Connector\Sabre\Principal;
+use OCP\App\IAppManager;
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\ILogger;
+use OCP\IServerContainer;
+
+class Application extends App implements IBootstrap {
+	public function __construct(array $urlParams = []) {
+		parent::__construct('nmc_sharing', $urlParams);
+	}
+
+	public function register(IRegistrationContext $context): void {
+		$context->registerCapability(Capabilities::class);
+
+		$context->registerServiceAlias('Expiration', Expiration::class);
+		$context->registerServiceAlias(ITrashManager::class, TrashManager::class);
+		/** Register $principalBackend for the DAV collection */
+		$context->registerServiceAlias('principalBackend', Principal::class);
+	}
+
+	public function boot(IBootContext $context): void {
+		\OCA\Files\App::getNavigationManager()->add(function () {
+			return [
+				'id' => 'nmc_sharing',
+				'appname' => 'nmc_sharing',
+				'script' => 'list.php',
+				];
+		});
+
+	}
+}
---
+++ list.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ *
+ * @author Kavita sonawane<kavita.sonawane@t-systems.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+use OCP\Share\IManager;
+
+$config = \OC::$server->getConfig();
+$userSession = \OC::$server->getUserSession();
+// TODO: move this to the generated config.js
+/** @var IManager $shareManager */
+$shareManager = \OC::$server->get(IManager::class);
+
+$showgridview = $config->getUserValue($userSession->getUser()->getUID(), 'files', 'show_grid', false);
+$isIE = OC_Util::isIe();
+
+// renders the controls and table headers template
+$tmpl = new OCP\Template('nmc_sharing', 'index', '');
+
+// gridview not available for ie
+$tmpl->assign('showgridview', $showgridview && !$isIE);
+$tmpl->printPage();
+
