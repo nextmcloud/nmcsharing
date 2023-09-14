4ac0ee3cf1bb8e5eb9c6ff22001e2a49e1c61bad
Aleksei Efremov <alex525e@proton.me>
2023-09-14 13:07:13 +0300

    Initialize nmcsharing

--- js/sharing.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import { translate as t, translatePlural as n } from '@nextcloud/l10n'
-
-Vue.prototype.t = t
-Vue.prototype.n = n
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
 	entry: {
-		sharing: './js/sharing.js',
+		sharing: './src/files_sharing_tab.js',
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
