a70f4e17f5f7ef897055aee37e98e2ad31da5baf
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2024-01-23 17:12:40 +0100

    fixed php code

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,15 +1,14 @@
 <?php
+
 namespace OCA\Nmcsharing\Controller;
 
-use OCP\IRequest;
-use OCP\AppFramework\Http\TemplateResponse;
-use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
+use OCP\IRequest;
 
 class PageController extends Controller {
 	private $userId;
 
-	public function __construct($AppName, IRequest $request, $UserId){
+	public function __construct($AppName, IRequest $request, $UserId) {
 		parent::__construct($AppName, $request);
 		$this->userId = $UserId;
 	}
