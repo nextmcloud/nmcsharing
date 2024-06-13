1bd82ea530161b3cd99f3f094f5ecea4588719fb
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2024-06-13 11:54:35 +0200

    added new file action button

---
+++ src/actions/sharingStatusAction.js
@@ -0,0 +1,58 @@
+import { FileAction, Permission } from '@nextcloud/files'
+import { translate as t } from '@nextcloud/l10n'
+
+import { action as sidebarAction } from './sidebarAction.js'
+
+export const action = new FileAction({
+	id: 'sharing-status-magentacloud',
+	displayName(nodes) {
+		const node = nodes[0]
+		const shareTypes = Object.values(node?.attributes?.['share-types'] || {}).flat()
+
+		if (shareTypes.length > 0) {
+			return t('files_sharing', 'Shared')
+		}
+
+		return ''
+	},
+
+	title(nodes) {
+		const node = nodes[0]
+
+		if (Array.isArray(node.attributes?.['share-types'])) {
+			return t('files_sharing', 'Shared multiple times with different people')
+		}
+
+		return t('files_sharing', 'Show sharing options')
+	},
+
+	iconSvgInline() {
+		return ''
+	},
+
+	enabled(nodes) {
+		if (nodes.length !== 1) {
+			return false
+		}
+
+		const node = nodes[0]
+		const isMixed = Array.isArray(node.attributes?.['share-types'])
+
+		if (isMixed) {
+			return true
+		}
+
+		return (node.permissions & Permission.SHARE) !== 0
+	},
+
+	async exec(node, view, dir) {
+		if ((node.permissions & Permission.READ) !== 0) {
+			window.OCA?.Files?.Sidebar?.setActiveTab?.('sharing')
+			return sidebarAction.exec(node, view, dir)
+		}
+		return null
+	},
+
+	inline: () => true,
+
+})
