ac7d47eb9de88e25fabb71a9a29b7a71e3ba2505
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2025-06-27 10:22:07 +0200

    Fixed DownloadLimitService.js

--- src/services/DownloadLimitService.js
+++ src/services/DownloadLimitService.js
@@ -2,18 +2,19 @@ import axios from '@nextcloud/axios'
 import { generateOcsUrl } from '@nextcloud/router'
 
 export const getDownloadLimit = async function(token) {
-	const response = await axios.get(generateOcsUrl('/apps/files_downloadlimit/api/v2/{token}/limit', { token }))
+	const url = generateOcsUrl(`/apps/files_downloadlimit/api/v1/${encodeURIComponent(token)}/limit`)
+	const response = await axios.get(url)
 	return response.data.ocs.data
 }
 
 export const setDownloadLimit = async function(token, limit) {
-	const response = await axios.put(generateOcsUrl('/apps/files_downloadlimit/api/v2/{token}/limit', { token }), {
-		limit,
-	})
+	const url = generateOcsUrl(`/apps/files_downloadlimit/api/v1/${encodeURIComponent(token)}/limit`)
+	const response = await axios.put(url, { limit })
 	return response.data.ocs.data
 }
 
 export const deleteDownloadLimit = async function(token) {
-	const response = await axios.delete(generateOcsUrl('/apps/files_downloadlimit/api/v2/{token}/limit', { token }))
+	const url = generateOcsUrl(`/apps/files_downloadlimit/api/v1/${encodeURIComponent(token)}/limit`)
+	const response = await axios.delete(url)
 	return response.data.ocs.data
 }
